b8d8e605cf741552c08d092223f6651f3161d3be
Drenn <camachodarence52@gmail.com>
2026-08-28 21:17:58 +0800

    Update input prompts for clarity

--- scal.py
+++ scal.py
@@ -1,5 +1,5 @@
-num1 = float(input("Type the first number: "))
-num2 = float(input("Type the second number: "))
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
 operation = input("Select the operation (+, -, *, /): ")
 
 if operation == "+":
