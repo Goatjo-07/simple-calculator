dfdbcb9e035c4a3efdb660b685099857cb0c3dd2
Drenn <camachodarence52@gmail.com>
2026-08-28 20:48:07 +0800

    Add basic calculator functionality in scal.py

---
+++ scal.py
@@ -0,0 +1,19 @@
+num1 = float(input("Type the first number: "))
+num2 = float(input("Type the second number: "))
+operation = input("Select the operation (+, -, *, /): ")
+
+if operation == "+":
+    result = num1 + num2
+elif operation == "-":
+    result = num1 - num2 
+elif operation == "*":
+    result = num1 * num2
+elif operation == "/":
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error: Division by zero"
+else:
+    result = "Error: Invalid operation"
+
+print("Answer: ", result)
